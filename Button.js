c86f0335d656dcfc2d623e0fb13751e1d32d2213
Kristofer Selbekk <selbeezy@gmail.com>
2018-09-09 19:51:30 +0200

    Add the presentation

---
+++ Button.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+const Button = styled.button`
+  padding: 10px 20px;
+  background-color: #ff5b5b;
+  color: white;
+  font-size: 16px;
+  border: 0;
+  appearance: none;
+  border-radius: 3px;
+  margin: 20px 0;
+`;
+
+export default Button;
--- theme.js
+++ theme.js
@@ -1,12 +1,36 @@
-import theme from 'mdx-deck/themes'
+import { dark as theme } from 'mdx-deck/themes';
 
 export default {
   ...theme,
+  colors: {
+    background: '#0e0e0e',
+    text: '#fff',
+    preBackground: '#464547',
+    code: '#ff5b5b',
+    link: '#fff',
+  },
+  h1: {
+    fontFamily: 'Newzald, serif',
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 400,
+  },
+  heading: {
+    fontFamily: '"DIN OT"',
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 300,
+  },
+  p: {
+    fontFamily: '"DIN OT Light"',
+    color: 'white',
+    fontSize: 15,
+    fontWeight: 400,
+  },
 
   // Customize your presentation theme here.
   //
   // Read the docs for more info:
   // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
   // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
-
-}
+};
